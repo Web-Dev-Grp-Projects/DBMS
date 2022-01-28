ab96a1a96471981d65744ade3712b2f751e3dba9
adityagoel28 <adityagoel2002@gmail.com>
2022-01-28 13:54:42 +0530

    deploy test

--- static/js/main1.js
+++ static/js/main1.js
@@ -1,84 +1,118 @@
-import * as THREE from 'https://cdn.skypack.dev/three';
-// import { MapControls, OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-// import { gl_Position } from '../shaders/vertex.glsl'
-// import vitePluginCdn from 'https://cdn.skypack.dev/vite-plugin-cdn';
-// import vertexShader from '../shaders/vertex.glsl' assert {type: 'octet-stream'}
-var renderer, vShader, fShader, camera, scene, atmosphereVShader, atmosphereFShader, sphere;
-var loader = new THREE.FileLoader();
-// var vShader;
-// vShader = document.getElementById('vertexShader').textContent;
-// console.log(vShader);
+console.clear();
+import simplexNoise from 'https://cdn.skypack.dev/simplex-noise';
+import * as THREE from "https://cdn.skypack.dev/three@0.132.2/build/three.module.js";
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js";
 
-init();
+THREE.BufferGeometry.prototype.tripleFace = tripleFace;
 
-function init(){
-  scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  
-  renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector("#bg"),
-    antialias: true
-  });
-  
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setPixelRatio(window.devicePixelRatio);
-  document.body.appendChild(renderer.domElement);
-  camera.position.z = 15;
+let scene = new THREE.Scene();
+scene.background = new THREE.Color(0x7f7f7f);
+let camera = new THREE.PerspectiveCamera(60, innerWidth / innerHeight, 1, 1000);
+camera.position.set(0, 0, 15);
+let renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(innerWidth, innerHeight);
+document.body.appendChild(renderer.domElement);
 
-  var numFilesLeft = 4;
-  
-  function runMoreIfDone() {
-     --numFilesLeft;
-     if (numFilesLeft === 0) {
-       more();
-     }
+let controls = new OrbitControls(camera, renderer.domElement);
+controls.enablePan = false;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 1;
+
+let light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.setScalar(1);
+scene.add(
+  light, 
+  new THREE.AmbientLight(0xffffff, 0.05),
+  new THREE.HemisphereLight( 0xff007f, 0x007fff)
+);
+
+let g = new THREE.IcosahedronGeometry(5, 20).tripleFace();
+let m = new THREE.MeshStandardMaterial({ 
+  color: 0x7f7f7f, 
+  metalness: 0.25,
+  roughness: 0.75
+});
+m.userData = {
+  uniforms: {
+    time: {value: 0} 
   }
-  
-  loader.load('../static/shaders/vertex.glsl', function ( data ) {vShader =  data; runMoreIfDone() ; },);
-  loader.load('../static/shaders/fragment.glsl', function ( data ) {fShader =  data; runMoreIfDone(); },);
-  loader.load('../static/shaders/atmosphereVertex.glsl', function ( data ) {atmosphereVShader =  data; runMoreIfDone() ; },);
-  loader.load('../static/shaders/atmosphereFragment.glsl', function ( data ) {atmosphereFShader =  data; runMoreIfDone(); },);
 }
+let o = new THREE.Mesh(g, m);
+scene.add(o);
 
-function more() {
-  // const normalTexture = new THREE.TextureLoader().load('normal.jpg');
-  
-  // Creating sphere and adding to scene.
-  sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(5, 50, 50),
-    new THREE.ShaderMaterial({
-      vertexShader: vShader,
-      fragmentShader: fShader,
-      uniforms: {
-        globeTexture: {
-          value: new THREE.TextureLoader().load('../static/images/earth1.jpg')
-        }
-      }
-    })
-  );
+window.addEventListener("resize", () => {
+  camera.aspect = innerWidth / innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(innerWidth, innerHeight);
+});
 
-  scene.add(sphere);
-  
-  // Creating atmosphere
-  const atmosphere = new THREE.Mesh(
-    new THREE.SphereGeometry(5, 50, 50),
-    new THREE.ShaderMaterial({
-      vertexShader: atmosphereVShader,
-      fragmentShader: atmosphereFShader,
-      blending: THREE.AdditiveBlending,
-      side: THREE.BackSide
-    })
-  );
-  
-  atmosphere.scale.set(1.1, 1.1, 1.1);
-  scene.add(atmosphere);
-  
-  animate();
-}
+let clock = new THREE.Clock();
 
-function animate() {
-  requestAnimationFrame(animate);
-  
+renderer.setAnimationLoop(() => {
+  let t = clock.getElapsedTime() * 0.1;
+  m.userData.uniforms.time.value = t;
+  controls.update();
   renderer.render(scene, camera);
-  sphere.rotation.y += 0.001;
+});
+
+function tripleFace() {
+  let geometry = this;
+  let pos = geometry.attributes.position;
+  if (geometry.index != null) {
+    console.log("Works for non-indexed geometries!");
+    return;
+  }
+
+  let facesCount = pos.count / 3;
+
+  let pts = [];
+  let onMove = [];
+  let triangle = new THREE.Triangle();
+  let a = new THREE.Vector3(),
+    b = new THREE.Vector3(),
+    c = new THREE.Vector3();
+  for (let i = 0; i < facesCount; i++) {
+    a.fromBufferAttribute(pos, i * 3 + 0);
+    b.fromBufferAttribute(pos, i * 3 + 1);
+    c.fromBufferAttribute(pos, i * 3 + 2);
+    triangle.set(a, b, c);
+    let o = new THREE.Vector3();
+    triangle.getMidpoint(o);
+
+    // make it tetrahedron-like
+    let l = a.distanceTo(b);
+    let h = 0; //(Math.sqrt(3) / 2) * l; // scale it at your will
+    let d = o.clone().normalize().setLength(h);
+    o.add(d);
+
+    pts.push(
+      o.clone(), a.clone(), b.clone(),
+      o.clone(), b.clone(), c.clone(),
+      o.clone(), c.clone(), a.clone()
+    );
+    onMove.push(1, 0, 0, 1, 0, 0, 1, 0, 0);
+  }
+
+  let g = new THREE.BufferGeometry().setFromPoints(pts);
+  g.setAttribute("onMove", new THREE.Float32BufferAttribute(onMove, 1));
+  g.computeVertexNormals();
+  setupAttributes(g);
+  return g;
+}
+
+function setupAttributes(geometry) {
+  const vectors = [
+    new THREE.Vector3(1, 0, 0),
+    new THREE.Vector3(0, 1, 0),
+    new THREE.Vector3(0, 0, 1)
+  ];
+
+  const position = geometry.attributes.position;
+  const centers = new Float32Array(position.count * 3);
+
+  for (let i = 0, l = position.count; i < l; i++) {
+    vectors[i % 3].toArray(centers, i * 3);
+  }
+
+  geometry.setAttribute("center", new THREE.BufferAttribute(centers, 3));
 }
