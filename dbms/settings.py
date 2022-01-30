b957dbe6ab94c9cf36a803e6255d26f18c5bdd5a
adityagoel28 <adityagoel2002@gmail.com>
2022-01-30 21:55:42 +0530

    Update settings.py

--- dbms/settings.py
+++ dbms/settings.py
@@ -15,8 +15,8 @@ import os
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
