5fd7db92123677e13220c3ec8e1ecdadb85f19c3
unknown <sanskarchandra26@gmail.com>
2022-02-07 17:54:58 +0530

    update

--- Home/views.py
+++ Home/views.py
@@ -14,28 +14,34 @@ from Home.models import *
 from django.db.models import F
 
 # Create your views here.
+
+
 def homePage(request):
     if request.method == 'POST':
         pass
     else:
         return render(request, "index.html", {})
 
+
 def loginPage(request):
     return render(request, "login.html", {})
+
 
 def customerwelcome(request):
     if not request.user.is_authenticated:
         return redirect(homePage)
 
     username = request.user.username
-    context = {'username': username, 'hotels': VaccineCenterDetails.objects.all()}
+    context = {'username': username,
+               'hotels': VaccineCenterDetails.objects.all()}
     return render(request, 'customerwelcome.html', context)
+
 
 def userauthenticate(request):
     username = request.POST['username']
     password = request.POST['password']
 
-    user = authenticate(username = username, password = password)
+    user = authenticate(username=username, password=password)
 
     if user is not None:
         login(request, user)
@@ -46,6 +52,7 @@ def userauthenticate(request):
         messages.add_message(request, messages.ERROR, "Invalid Credentials")
         return redirect(homePage)
 
+
 def signupuser(request):
     username = request.POST['username']
     password = request.POST['password']
@@ -53,31 +60,37 @@ def signupuser(request):
 
     # checking various conditions before signing up the user
     if password == repass:
-        if User.objects.filter(username = username).exists():
-            messages.add_message(request, messages.ERROR, "User already exists")
+        if User.objects.filter(username=username).exists():
+            messages.add_message(request, messages.ERROR,
+                                 "User already exists")
             return redirect(homePage)
 
-        User.objects.create_user(username = username, password = password).save()
-        messages.add_message(request, messages.SUCCESS, "User Successfully created")
+        User.objects.create_user(username=username, password=password).save()
+        messages.add_message(request, messages.SUCCESS,
+                             "User Successfully created")
 
     else:
         messages.add_message(request, messages.ERROR, "Password do not match")
 
     return redirect(homePage)
 
+
 def logoutuser(request):
-    logout(request) # logging out the user
+    logout(request)  # logging out the user
     return redirect(homePage)
 
+
 def vaccineDetails(request):
-    context = {'vaccineDet':VaccineDetails.objects.all()}
+    context = {'vaccineDet': VaccineDetails.objects.all()}
     return render(request, 'display.html', context)
+
 
 def book(request, bookp):
     vaccineid = bookp
     username = request
     context = {'vaccineid': vaccineid, 'username': username}
     return render(request, "form.html", context)
+
 
 def bookingdone(request):
     username = request.user.username
@@ -87,14 +100,19 @@ def bookingdone(request):
     date = request.POST['date']
     vaccine = request.POST['vaccineid']
 
-    VaccineDetails.objects.filter(Vaccine_ID = vaccine).update(availability = F('availability') - 1)
+    VaccineDetails.objects.filter(Vaccine_ID=vaccine).update(
+        availability=F('availability') - 1)
 
-    BookingDetails(username = username, aadhar = aadhar, name = name, date = date, vaccine = vaccine).save()
-    messages.add_message(request, messages.SUCCESS,'Vaccine Booked Successfully')
+    BookingDetails(username=username, aadhar=aadhar, name=name,
+                   date=date, vaccine=vaccine).save()
+    messages.add_message(request, messages.SUCCESS,
+                         'Vaccine Booked Successfully')
     return redirect(customerwelcome)
 
+
 def userbooking(request):
-    bookings = BookingDetails.objects.filter(username = request.user.username) # getting the booking database of the particular user
+    # getting the booking database of the particular user
+    bookings = BookingDetails.objects.filter(username=request.user.username)
     # info = VaccineDetails.objects.filter(Vaccine_ID = bookings.vaccine)  # diff bw get and filter?
     # vaccineid = info.Vaccine_ID
     # info = BookingDetails.objects.get(username = request.user.username) # getting the booking database of the particular user
@@ -103,23 +121,32 @@ def userbooking(request):
 
     context = {'bookings': bookings}
 
-    return render(request,'userbooking.html', context)
+    return render(request, 'userbooking.html', context)
+
 
 def search(request):
     pincode = request.POST['pincode']
-    context = {'vaccineDet':VaccineDetails.objects.filter(Center__pin_code = pincode)} # Filtering on the basis of foreign key atttribute
+    # Filtering on the basis of foreign key atttribute
+    context = {'vaccineDet': VaccineDetails.objects.filter(
+        Center__pin_code=pincode)}
     return render(request, 'display.html', context)
 
+
 def cancel(request, bookid):
-    messages.add_message(request, messages.SUCCESS, "Booking successfully cancelled")
-    bookings = BookingDetails.objects.filter(username = request.user.username, id = bookid)
+    messages.add_message(request, messages.SUCCESS,
+                         "Booking successfully cancelled")
+    bookings = BookingDetails.objects.filter(
+        username=request.user.username, id=bookid)
     # print(bookings[0])
     vaccinenum = bookings[0].vaccine
 
-    BookingDetails.objects.filter(id = bookid).delete() # cancelling the user booking by deleteing the booking from his account
-    VaccineDetails.objects.filter(Vaccine_ID = vaccinenum).update(availability = F('availability') + 1)
+    # cancelling the user booking by deleteing the booking from his account
+    BookingDetails.objects.filter(id=bookid).delete()
+    VaccineDetails.objects.filter(Vaccine_ID=vaccinenum).update(
+        availability=F('availability') + 1)
 
     return redirect(userbooking)
+
 
 def pdf(request):
     username = request.user.username
@@ -140,14 +167,15 @@ def pdf(request):
 
     # p.setStrokeColor(red)
     p.setFillColor(green)
-    p.drawString(100, 800, "CONGRATULATIONS "+ username + ", YOU ARE VACCINATED!") # why not commma
+    p.drawString(100, 800, "CONGRATULATIONS " + username +
+                 ", YOU ARE VACCINATED!")  # why not commma
     # Close the PDF object cleanly, and we're done.
-    p.drawImage(img, 150, 200, width = 320, preserveAspectRatio = True, mask='auto')
+    p.drawImage(img, 150, 200, width=320,
+                preserveAspectRatio=True, mask='auto')
 
     p.showPage()
     p.save()
 
     # FileResponse sets the Content-Disposition header so that browsers present the option to save the file.
     buffer.seek(0)
-    return FileResponse(buffer, as_attachment=True, filename = 'Vaccination.pdf')
-
+    return FileResponse(buffer, as_attachment=True, filename='Vaccination.pdf')
